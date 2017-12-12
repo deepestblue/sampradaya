4efd11d14b7c1d4f83f16024448e28932c362c50
Ambarish Sridharanarayanan <ambarish@ksharanam.net>
2017-12-12 12:46:05 -0800

    Renders fixed text onto hard-coded PNG.

--- test/main.cpp
+++ test/main.cpp
@@ -1,45 +1,52 @@
+#include <string>
 #include "QtWidgets/QApplication"
 #include "QtGui/QImage"
 #include "QtGui/QPainter"
 
+using namespace std;
+
 template<typename T>
 void throw_if_false(T exp) {
-    if (exp)
+    if (! exp)
         throw "Ack!";
 }
 
-void drawText(
-    const QString &typefaceName,
-    int x, int y, const QString &text,
+void draw_text_at(
+    const string &text,
+    int x, int y,
     QPainter &painter) {
 
-    QFont font(typefaceName);
-    painter.setFont(font);
-    painter.drawText(x, y, text);
+    painter.drawText(x, y, QString::fromStdString(text));
 }
 
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-
+void render_line_to_image(const string &line, const string &output_filename) {
     QPainter painter;
     QImage dummy;
 
     QImage image(200, 200, QImage::Format_RGB32);
     image.fill(Qt::white);
 
-    {
-        painter.begin(&image);
+    throw_if_false(
+        painter.begin(&image)
+        );
 
-        drawText("Arial", 10, 10, "My name is Ambarish.", painter);
-        drawText("Arial", 10, 30, "எனது பெயர் அம்பரீடன்.", painter);
-        drawText("Sampradaya", 10, 50, "𑌮𑌮 𑌨𑌾𑌮 𑌅𑌮𑍍𑌬𑌰𑍀𑌷𑌃 ।", painter);
+    QFont font("Sampradaya");
+    painter.setFont(font);
 
-        painter.end();
-    }
+    draw_text_at(line, 10, 50, painter);
 
-    if (! image.save("foo.png", "PNG")) {
-        throw "Ack!";
-    }
+    throw_if_false(
+        painter.end()
+        );
 
+    throw_if_false(
+        image.save(QString::fromStdString(output_filename), "PNG")
+        );
+}
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    render_line_to_image("𑌮𑌮 𑌨𑌾𑌮 𑌅𑌮𑍍𑌬𑌰𑍀𑌷𑌃 ।", "foo.png");
     return 0;
 }
