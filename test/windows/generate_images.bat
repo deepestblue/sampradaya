3f67370b06a1f42cfa923ef437b081262b56c842
Ambarish Sridharanarayanan <ambarish@ksharanam.net>
2019-04-16 22:07:34 -0700

    Adding consonant-ligature tests.

--- test/windows/generate_images.bat
+++ test/windows/generate_images.bat
@@ -15,3 +15,5 @@ IF NOT EXIST master_images\post_ya_post_repha MD master_images\post_ya_post_reph
 rasterise_text ..\cases\post_ya_post_repha.txt master_images\post_ya_post_repha
 IF NOT EXIST master_images\pre_repha_half_form MD master_images\pre_repha_half_form
 rasterise_text ..\cases\pre_repha_half_form.txt master_images\pre_repha_half_form
+IF NOT EXIST master_images\consonant_vowel_ligatures MD master_images\consonant_vowel_ligatures
+rasterise_text ..\cases\consonant_vowel_ligatures.txt master_images\consonant_vowel_ligatures
