c340151871be70ed4b408e72129c09e2743acf69
Ambarish Sridharanarayanan <ambarish@ksharanam.net>
2019-04-16 22:33:39 -0700

    More pre-repha test-cases; split them into case-files.

--- test/windows/generate_images.bat
+++ test/windows/generate_images.bat
@@ -13,7 +13,9 @@ IF NOT EXIST master_images\o_au_rearrangement MD master_images\o_au_rearrangemen
 rasterise_text ..\cases\o_au_rearrangement.txt master_images\o_au_rearrangement
 IF NOT EXIST master_images\post_ya_post_repha MD master_images\post_ya_post_repha
 rasterise_text ..\cases\post_ya_post_repha.txt master_images\post_ya_post_repha
-IF NOT EXIST master_images\pre_repha_half_form MD master_images\pre_repha_half_form
-rasterise_text ..\cases\pre_repha_half_form.txt master_images\pre_repha_half_form
+IF NOT EXIST master_images\pre_repha_with_vowel_matras MD master_images\pre_repha_with_vowel_matras
+rasterise_text ..\cases\pre_repha_with_vowel_matras.txt master_images\pre_repha_with_vowel_matras
+IF NOT EXIST master_images\pre_repha_complex_cases MD master_images\pre_repha_complex_cases
+rasterise_text ..\cases\pre_repha_complex_cases.txt master_images\pre_repha_complex_cases
 IF NOT EXIST master_images\consonant_vowel_ligatures MD master_images\consonant_vowel_ligatures
 rasterise_text ..\cases\consonant_vowel_ligatures.txt master_images\consonant_vowel_ligatures
