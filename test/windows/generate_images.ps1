513dfb065477786a746bd135f26ba2d8bd3080a7
Ambarish Sridharanarayanan <5531915+deepestblue@users.noreply.github.com>
2020-03-08 15:59:51 -0700

    New PowerShell generate_images script on Windows

---
+++ test/windows/generate_images.ps1
@@ -0,0 +1,23 @@
+#cl "$PSScriptRoot/rasterise_text.cxx" /EHsc /std:c++latest /nologo /W4 d2d1.lib dwrite.lib  /Fo: "$PSScriptRoot/" /Fe: "$PSScriptRoot/" | Out-Null
+
+function MkDirIfNotExists() {
+    Param([Parameter(Mandatory = $True)] [String] $DirectoryToCreate)
+    if (Test-Path -LiteralPath $DirectoryToCreate) {
+        return
+    }
+    New-Item -Path $DirectoryToCreate -ItemType Directory -ErrorAction Stop | Out-Null
+}
+
+if ($args.Count -lt 1) {
+    throw "Required argument missing"
+}
+
+$outputRoot = "$((Get-Location).Path)\$($args[0])"
+MkDirIfNotExists $outputRoot
+
+Get-ChildItem "$PSScriptRoot/../cases" -Name *txt | ForEach-Object {
+    $dirName = $_ -replace '.txt', ''
+    $outputDir = "$outputRoot/$dirName"
+    MkDirIfNotExists $outputDir
+    & "$PSScriptRoot/rasterise_text" "$PSScriptRoot/../cases/$_" $outputDir
+}
