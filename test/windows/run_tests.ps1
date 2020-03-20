7d4f53d22d8e9cbc520663786a06b898d20d4126
Ambarish Sridharanarayanan <5531915+deepestblue@users.noreply.github.com>
2020-03-19 18:49:29 -0700

    Stub for running tests in Windows

---
+++ test/windows/run_tests.ps1
@@ -0,0 +1,13 @@
+if ($args.Count -lt 1) {
+    throw "Required argument missing."
+}
+
+function MkDirIfNotExists() {
+    Param([Parameter(Mandatory = $True)] [String] $DirectoryToCreate)
+    if (Test-Path -LiteralPath $DirectoryToCreate) {
+        return
+    }
+    New-Item -Path $DirectoryToCreate -ItemType Directory -ErrorAction Stop | Out-Null
+}
+
+# TODO
