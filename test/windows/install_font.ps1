49cdf6268b2b5f411dca8a7f4ca8ed1dda8c2b33
Ambarish Sridharanarayanan <5531915+deepestblue@users.noreply.github.com>
2020-03-19 17:57:23 -0700

    Revert "Add new target to install font"
    This reverts commit 8e72ee037f532e93697503ba010687947954155a.

--- test/windows/install_font.ps1
+++
@@ -1,7 +0,0 @@
-$fontsFolder = (New-Object -ComObject Shell.Application).Namespace(0x14)
-if ($fontsFolder.ParseName('Sampradaya Regular')) {
-    return
-}
-
-$sampradayaPath = (Get-Item '../../src/windows/Sampradaya.ttf').fullname
-$fontsFolder.CopyHere($sampradayaPath, 4 + 16)
