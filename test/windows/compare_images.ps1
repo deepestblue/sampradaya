e6f67be6dec0c6a65349f9234d15decf35a1002a
Ambarish Sridharanarayanan <5531915+deepestblue@users.noreply.github.com>
2020-03-21 16:24:00 -0700

    Start comparing images

--- test/windows/compare_images.ps1
+++
@@ -1,4 +0,0 @@
-[Reflection.Assembly]::LoadFile("Z:\projects\Sampradaya\test\windows\TestApiCore.dll")
-
-$master = [Microsoft.Test.VisualVerification.Snapshot]::FromFile("Z:/projects/Sampradaya/test/windows/master_images/character_set/1.bmp");
-
--- test/windows/run_tests.ps1
+++ test/windows/run_tests.ps1
@@ -1,8 +1,13 @@
-$ErrorActionPreference="Stop"
+Param(
+    [Parameter(Mandatory=$true)][string]$master_images
+)
 
-if ($args.Count -lt 1) {
-    throw "Required argument missing."
-}
+$ErrorActionPreference="Stop"
+Add-Type -Assembly System.Drawing
+
+[Reflection.Assembly]::LoadFile("Z:\projects\Sampradaya\test\windows\TestApiCore.dll")
+
+$snapshotVerifier = New-Object -TypeName Microsoft.Test.VisualVerification.SnapshotColorVerifier -ArgumentList $([System.Drawing.Color]::Black), (New-Object Microsoft.Test.VisualVerification.ColorDifference)
 
 function MkDirIfNotExists() {
     Param([Parameter(Mandatory = $True)] [String] $DirectoryToCreate)
@@ -18,9 +23,21 @@ function New-TemporaryDirectory {
     New-Item -ItemType Directory -Path (Join-Path $parent $name)
 }
 
+function Compare-Images {
+Param(
+    [Parameter(Mandatory=$true)][string]$expectedPath,
+    [Parameter(Mandatory=$true)][string]$actualPath,
+    [Parameter(Mandatory=$true)][string]$diffPath
+)
+    $expected = [Microsoft.Test.VisualVerification.Snapshot]::FromFile($expectedPath);
+    $actual = [Microsoft.Test.VisualVerification.Snapshot]::FromFile($actualPath);
+
+    $diff = $actual.CompareTo($expected);
+}
+
 $tmpDir = New-TemporaryDirectory
 
-robocopy $($args[0]) "$tmpDir/expected" /MIR /Z /UNICODE /NFL /NDL /NP /NJH /NJS /NS /NC
+robocopy $master_images "$tmpDir/expected" /MIR /Z /UNICODE /NFL /NDL /NP /NJH /NJS /NS /NC
 
 MkDirIfNotExists "$tmpDir/actual"
 ./generate_images.ps1 "$tmpDir/actual"
@@ -29,7 +46,16 @@ MkDirIfNotExists "$tmpDir/diff"
 
 Get-ChildItem -Path "$tmpDir/actual" -Recurse -File | ForEach-Object {
     $actual = $_.FullName
+    $tmpPath = $tmpDir.FullName
+    $expected = $actual.replace($tmpPath + '\actual\', $tmpPath + '\expected\')
+    $diff = $actual.replace($tmpPath + '\actual\', $tmpPath + '\diff\')
 
+    if (! (Compare-Object $(Get-Content $expected) $(Get-Content $actual))) {
+#        Compare-Images $expected $actual $diff
+        return
+    }
+
+    Compare-Images $expected $actual $diff
 }
 
 #Get-ChildItem "$tmpDir/diff"
