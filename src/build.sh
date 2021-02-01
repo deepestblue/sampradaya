acded82fe683a0757517ffedc21ed49806660fe3
Ambarish Sridharanarayanan <5531915+deepestblue@users.noreply.github.com>
2021-01-31 20:45:35 -0800

    Misc. fixes to github workflow

--- src/build.sh
+++ src/build.sh
@@ -3,5 +3,5 @@
 typefacename=$1
 
 $(dirname $0)/sfd2ttf.pe ${typefacename%.ttf}.sfd $typefacename
-gftools-fix-unwanted-tables.py --tables FFTM $typefacename
+gftools fix-unwanted-tables.py --tables FFTM $typefacename
 gftools fix-dsig --autofix $typefacename
